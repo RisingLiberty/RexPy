ddd39be57effc80be9d607eadb6c7c6297249f32
Nick De Breuck <nick.debreuck@outlook.com>
2023-02-07 12:34:05 +0000

    clang-tidy first pass now auto fixes issues

--- rexpy/run_clang_tools.py
+++ rexpy/run_clang_tools.py
@@ -31,7 +31,7 @@ def run(projectName, compdb, srcRoot):
   clang_config_file = os.path.join(rexpy.util.find_in_parent(srcRoot, ".clang-tidy_first_pass"), ".clang-tidy_first_pass")
 
   rexpy.diagnostics.log_info("Running clang-tidy - auto fixes")
-  rc = __run_command(f"py {script_path}/run_clang_tidy.py -clang-tidy-binary={clang_tidy_path} -clang-apply-replacements-binary={clang_apply_replacements_path} -config-file={clang_config_file} -p={compdb} -header-filter=.* -quiet") # force clang compiler, as clang-tools expect it
+  rc = __run_command(f"py {script_path}/run_clang_tidy.py -clang-tidy-binary={clang_tidy_path} -clang-apply-replacements-binary={clang_apply_replacements_path} -config-file={clang_config_file} -p={compdb} -header-filter=.* -quiet -fix") # force clang compiler, as clang-tools expect it
 
   if rc != 0:
     raise Exception("clang-tidy auto fixes failed")
