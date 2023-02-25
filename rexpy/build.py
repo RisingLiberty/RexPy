7a48a3ba4a8365f4f450a56cc87d294a09b2b8e4
Nick De Breuck <nick.debreuck@outlook.com>
2023-02-25 10:32:02 +0000

    Revert "improving build.py so it can now build based on config, project and compiler"
    This reverts commit 245699ef691789f20a90d184f72205b52ad897a4.

--- rexpy/build.py
+++ rexpy/build.py
@@ -1,20 +1,10 @@
 import os
 import rexpy.required_tools
-import rexpy.rex_json
-import rexpy.util
 
 tool_paths_dict = rexpy.required_tools.tool_paths_dict
-
-def new_build(project, config, compiler, shouldClean):
-  root = rexpy.util.find_root()
-  settings = rexpy.rex_json.load_file(os.path.join(root, "build", "config", "settings.json"))
-  intermediate_folder = settings["intermediate_folder"]
-  build_folder = settings["build_folder"]
-  ninja_filename = f"{project}.{config}.{compiler}.ninja"
-  ninja_filepath = os.path.join(root, intermediate_folder, build_folder, "ninja", project, "ninja", ninja_filename)
-
+def new_build(ninjaFile, shouldClean):
   ninja_path = tool_paths_dict["ninja_path"]
   if shouldClean:
-    os.system(f"{ninja_path} -f {ninja_filepath} -t clean")
+    os.system(f"{ninja_path} -f {ninjaFile} -t clean")
 
-  return os.system(f"{ninja_path} -f {ninja_filepath}")
+  return os.system(f"{ninja_path} -f {ninjaFile}")
