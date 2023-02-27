1ac717c03a4794b584c2d1c8b1012b0bde389765
Nick De Breuck <nick.debreuck@outlook.com>
2023-02-27 23:06:21 +0000

    task raii printing now also prints the time it took to run the task

--- rexpy/task_raii_printing.py
+++ rexpy/task_raii_printing.py
@@ -1,14 +1,18 @@
-import diagnostics
+import time
+import rexpy.diagnostics
 
 class TaskRaiiPrint(object):
   def __init__(self, msg):
     self._msg = msg
     self._finished_msg = "done"
+    self.start_time = time.time()
 
-    diagnostics.log_info(msg)
+    rexpy.diagnostics.log_info(msg)
   
   def failed(self):
     self._finished_msg = "failed"
 
   def __del__(self):
-    diagnostics.log_info(f"{self._msg} - {self._finished_msg}")
+    end_time = time.time()
+    rexpy.diagnostics.log_info(f"{self._msg} - {self._finished_msg}")
+    rexpy.diagnostics.log_info(f"took {end_time - self.start_time:0.2f} seconds")
