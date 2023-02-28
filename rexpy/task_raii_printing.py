1ba1af759656524bedfce98e53e7ff0fff63f8de
Nick De Breuck <nick.debreuck@outlook.com>
2023-02-28 21:42:08 +0000

    the tab in task print is replaced by 2 spaces

--- rexpy/task_raii_printing.py
+++ rexpy/task_raii_printing.py
@@ -15,4 +15,4 @@ class TaskRaiiPrint(object):
   def __del__(self):
     end_time = time.time()
     rexpy.diagnostics.log_info(f"{self._msg} - {self._finished_msg}")
-    rexpy.diagnostics.log_info(f"\ttook {end_time - self.start_time:0.2f} seconds")
+    rexpy.diagnostics.log_info(f"  took {end_time - self.start_time:0.2f} seconds")
