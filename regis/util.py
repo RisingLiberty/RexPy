216dfc639c3de05bd866ce60ee54bf61a284733e
Nick De Breuck <nick.debreuck@outlook.com>
2023-03-09 19:24:16 +0000

    updating to version 0.1.11

--- regis/util.py
+++ regis/util.py
@@ -139,7 +139,6 @@ def is_windows():
 
 def run_subprocess(command):
   proc = subprocess.Popen(command)
-  proc.communicate()
   return proc
 
 def run_subprocess_with_working_dir(command, workingDir):
