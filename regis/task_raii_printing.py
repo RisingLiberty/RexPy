3d1bb9b02177057c03ef99a05a0e609a75436815
Nick De Breuck <nick.debreuck@outlook.com>
2023-11-20 00:55:07 +0000

    changing task raii printing to be used with "with" statements

--- regis/task_raii_printing.py
+++ regis/task_raii_printing.py
@@ -6,13 +6,17 @@ class TaskRaiiPrint(object):
     self._msg = msg
     self._finished_msg = "done"
     self.start_time = time.time()
-
-    regis.diagnostics.log_info(msg)
   
   def failed(self):
     self._finished_msg = "failed"
 
-  def __del__(self):
+  def __enter__(self):
+    regis.diagnostics.log_info(self._msg)
+    return self
+
+  def __exit__(self, exc_type, exc_value, traceback):
     end_time = time.time()
     regis.diagnostics.log_info(f"{self._msg} - {self._finished_msg}")
     regis.diagnostics.log_info(f"  took {end_time - self.start_time:0.2f} seconds")
+
+    
